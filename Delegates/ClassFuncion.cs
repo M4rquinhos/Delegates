187411a68903bec2f6c982d9439188b627c5c2be
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-01-20 15:41:51 -0600

    Agregar archivos de proyecto.

---
+++ Delegates/ClassFuncion.cs
@@ -0,0 +1,16 @@
+﻿namespace Delegates
+{
+    public class ClassFuncion
+    {
+
+        public string func1(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        public string func2(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Delegates;
+
+var objeto = new ClassFuncion();
+
+MiDelegate f = objeto.func1;
+Console.WriteLine($"El resultado es: {f(3, 4)}");
+
+f = objeto.func2;
+Console.WriteLine($"El resultado es: {f(3, 4)}");
+
+Console.WriteLine($"");
+public delegate string MiDelegate(int arg1, int arg2);
+
